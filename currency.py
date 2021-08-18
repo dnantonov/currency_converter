7ddbfcef1451a43acb74a243c179102607a82bb5
dnantonov <dnantonov@etu.stud.ru>
2021-08-18 17:38:25 +0300

    get_currency parser has been implemented

---
+++ currency.py
@@ -0,0 +1,21 @@
+import http
+import http.client
+import logging
+import re
+import sys
+import requests
+from bs4 import BeautifulSoup
+
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
+
+def get_currency(dollars):
+    logging.info(f'Converting {dollars}$ in rubles...')
+    response = requests.get('https://www.banki.ru/products/currency/usd/')
+    rate = re.findall(b'<div class="currency-table__large-text">(\d+(?:\,\d+)?)</div>', response.content)
+    formatted_rate = rate[0].decode('utf8').replace(',', '.')
+    logging.info(f'Dollar rate in rubles: {formatted_rate}$')
+    result_in_rubles = round(float(formatted_rate) * dollars, 2)
+    logging.info(f'{dollars}$ will be {result_in_rubles}₽ in rubles')
+    return result_in_rubles
+
--- server.py
+++ server.py
@@ -1,6 +1,9 @@
+import cgi
 import json
 import logging
 from http.server import BaseHTTPRequestHandler, HTTPServer
+
+from currency import get_currency
 
 
 class Server(BaseHTTPRequestHandler):
@@ -14,14 +17,37 @@ class Server(BaseHTTPRequestHandler):
 
     def do_GET(self):
         self._set_headers()
-        response = json.dumps({'hello': 'world', 'received': 'ok'})
+        rate = get_currency(1)
+        self._set_headers()
+        response = json.dumps({'amount': 1, 'rubles': rate, 'currency': 'american dollar'})
+        self.wfile.write(bytes(response, "utf-8"))
+
+    def do_POST(self):
+        ctype, pdict = cgi.parse_header(self.headers.get('content-type'))
+
+        if ctype != 'application/json':
+            self.send_response(400)
+            self.end_headers()
+            return
+
+        length = int(self.headers.get('content-length'))
+        data = self.rfile.read(length).decode('utf-8')
+        message = json.loads(data)
+        rate = get_currency(message['amount'])
+
+        message['rubles'] = rate
+        message['currency'] = 'american dollar'
+
+        self._set_headers()
+        response = json.dumps(message)
+        self._set_headers()
         self.wfile.write(bytes(response, "utf-8"))
 
 
 def run(server_class=HTTPServer, handler_class=Server, port=8080):
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
-    print(f'Starting httpd on port {port}')
+    logging.info(f'Starting httpd on port {port}')
     httpd.serve_forever()
 
 
